0a0f4a69f2d139afb3e7f9cad550459d5edbbe91
Hari Prasad <101968972+meeraj-boot@users.noreply.github.com>
2023-11-03 01:55:28 +0530

    Update crude.py

--- crude.py
+++ crude.py
@@ -7,7 +7,6 @@
 # Import necessary libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
